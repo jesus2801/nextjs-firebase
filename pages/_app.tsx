54ebc7cc34a35d0cfb82d03500f41ecf5db4efb0
jesus2801 <jesusdavid280147@gmail.com>
2021-03-19 08:11:35 -0500

    app 1.0.0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import UseAppContext from '../firebase/index';
 
 import '../styles/globals.scss';
 
+// app 1.0.0
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UseAppContext>
